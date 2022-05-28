66a157fe349e6956890e568949b994f18a03c65a
katusha-2010 <katusha-2010@mail.ru>
2022-05-28 21:33:21 +0300

    refactor: replace tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
